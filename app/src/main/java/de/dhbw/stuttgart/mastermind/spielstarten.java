f448a6f103ab2dd948df331bcaa06809c3d5b230
anja <Anja.Ohlhaeuser@googlemail.com>
2017-04-02 15:11:03 +0200

    Layout

---
+++ app/src/main/java/de/dhbw/stuttgart/mastermind/spielstarten.java
@@ -0,0 +1,13 @@
+package de.dhbw.stuttgart.mastermind;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class spielstarten extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spielstarten);
+    }
+}
