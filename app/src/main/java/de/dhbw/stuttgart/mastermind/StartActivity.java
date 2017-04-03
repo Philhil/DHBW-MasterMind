659b9bbe9cfeea2c2fd576fc77b2a6191eb33df4
admin <admin@Gasts-Air.fritz.box>
2017-04-03 22:39:14 +0200

    init StartActivity

---
+++ app/src/main/java/de/dhbw/stuttgart/mastermind/StartActivity.java
@@ -0,0 +1,57 @@
+package de.dhbw.stuttgart.mastermind;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.TextView;
+
+public class StartActivity extends AppCompatActivity {
+
+    private Fragment fragment;
+    private FragmentManager fragmentManager;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new HomeFragment();
+                    break;
+                case R.id.navigation_highscore:
+                    fragment = new HighscoreFragment();
+                    break;
+                case R.id.navigation_settings:
+                    fragment = new SettingsFragment();
+                    break;
+            }
+            final FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.replace(R.id.content, fragment).commit();
+
+            return true;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        fragmentManager = getSupportFragmentManager();
+        fragment = new HomeFragment();
+        final FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(R.id.content, fragment).commit();
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
