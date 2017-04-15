8ab1cabe2566bd7337c1be217d12be8e96a03c3f
anja <anja.ohlhaeuser@googlemail.com>
2017-04-15 16:00:26 +0200

    Test 2.0

---
+++ app/src/main/java/de/dhbw/stuttgart/mastermind/GameBreak.java
@@ -0,0 +1,13 @@
+package de.dhbw.stuttgart.mastermind;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameBreak extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_break);
+    }
+}
